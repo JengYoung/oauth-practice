fcac03c7fbc4a47ecf7c1236e2b61db85eb8a46e
JengYoung <young_pallete@naver.com>
2022-08-07 18:36:25 +0900

    :package: add react-router-dom, set naver-login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
+import NaverLoginPage from "./pages/NaverLoginPage";
+
 function App() {
 
   return (
     <div className="App">
-      T.B.D. Naver Login
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={< IndexPage />}></Route>
+          <Route path="/naver-login" element={< NaverLoginPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/NaverLoginPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NaverLoginPage = () => {
+  return (
+    <div>NaverLoginPage</div>
+  )
+}
+
+export default NaverLoginPage
