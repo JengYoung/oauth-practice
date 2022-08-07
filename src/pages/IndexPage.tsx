f7e3bd9dd1f49471ca744e42118c0fbd4e51ed62
JengYoung <young_pallete@naver.com>
2022-08-07 19:45:16 +0900

    :sparkles: 네이버 로그인 연동 API 기능 추가
    공식문서 참고
    https://developers.naver.com/docs/login/api/api.md

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+
+import React, { useEffect } from 'react'
+
+declare global {
+  interface Window {
+    naver: any;
+  }
+}
 
 const IndexPage = () => {
+  // const url = window.opener.document.location.href; 
+  const initializeNaverLogin = () => {
+    const callbackUrl = `http://localhost:3000/naver-login`;
+    const naverLogin = new window.naver.LoginWithNaverId({
+      clientId: process.env.REACT_APP_NAVER_CLIENT_ID,
+      callbackUrl,
+      isPopup: true,
+      callbackHandle: true,
+      loginButton: { color: 'white', type: 2, height: '45'}
+
+    });
+
+    naverLogin.init();
+    
+    naverLogin.getLoginStatus(async function (status) {
+      if (status) {
+        console.log(naverLogin.user)
+      }
+    })
+  }
+
+  useEffect(() => {
+    initializeNaverLogin()
+  })
+
   return (
-    <div>IndexPage</div>
+    <div id="naverIdLogin">IndexPage</div>
   )
 }
 
--- src/pages/NaverLoginPage.tsx
+++ src/pages/NaverLoginPage.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+
+import React, { useEffect } from 'react'
+
+declare global {
+  interface Window {
+    naver: any;
+  }
+}
 
 const NaverLoginPage = () => {
+
+  
+  const getToken = () => {
+    const token = window.location.href.split('=')[1].split('&')[0]  
+
+    localStorage.setItem('access_token', token)
+  }
+
+  const getUserAccessToken = () => {
+    window.location.href.includes('access_token') && getToken();
+  }
+
+  useEffect(() => {
+    getUserAccessToken();
+  })
+
   return (
-    <div>NaverLoginPage</div>
+    <div id="naverIdLogin">NaverLoginPage</div>
   )
 }
 
