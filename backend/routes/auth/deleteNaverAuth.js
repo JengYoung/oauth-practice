05453b87dce0e6fb9891a909dd80da11b832a3b7
JengYoung <young_pallete@naver.com>
2022-08-09 16:54:39 +0900

    :sparkles: API 연동 해제 추가

--- backend/routes/auth/deleteNaverAuth.js
+++ backend/routes/auth/deleteNaverAuth.js
@@ -1,17 +1,15 @@
 import axios from "axios";
 
 const deleteNaverAuth = async (req, res) => {
-    const authorizationCode = req.headers.authorization;
-    console.log(authorizationCode)
-    if (!authorizationCode) res.send(401, 'Unauthorized access!');
+    // const authorizationCode = req.headers.authorization;
+    // if (authorizationCode === undefined) res.status(401).send('Unauthorized access!');
+
+    const accessToken = req.body.data.accessToken;
+    console.log('access_token: ', accessToken)
 
     try {
-        const result = await axios.get('https://openapi.naver.com/v1/nid/me', {
-            headers: {
-                Authorization: authorizationCode,
-            }
-        })
-
+        const result = await axios.post(`https://nid.naver.com/oauth2.0/token?grant_type=delete&client_id=${process.env.NAVER_CLIENT_ID}&client_secret=${process.env.NAVER_SECRET}&access_token=${accessToken}&service_provider=NAVER`);
+        console.log(result)
         return res.send(result.data)
     } catch(e) {
         console.log(e)
