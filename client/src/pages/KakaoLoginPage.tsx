24934e1261825aa2910511b222b329d916a01127
JengYoung <young_pallete@naver.com>
2022-08-23 15:00:22 +0900

    :recycle: StrictMode는 유지한 채로, 두 번 API 호출하는 현상 해결
    문제: code는 한 번 호출되면 이후 토큰 발급에 사용할 수 없음.
    해결: useRef를 사용하여 리렌더링을 발생시키지 않도록 하여 상태 값을 잃지 않도록 수정하여 요청 여부 체크

--- client/src/pages/KakaoLoginPage.tsx
+++ client/src/pages/KakaoLoginPage.tsx
@@ -1,20 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { useSearchParams } from "react-router-dom";
 import getAuthToken from "../api/auth/kakao/getAuthToken";
 
 const KakaoLoginPage = () => {
   const [params] = useSearchParams();
+  const isRequested = useRef(false);
 
   useEffect(() => {
+    if (isRequested.current) return;
     async function getToken() {
       const code = params.get("code");
       const res = await getAuthToken(code);
       console.log(res);
 
       // window.localStorage.setItem("ACCESS_TOKEN", JSON.stringify(token));
+      isRequested.current = true;
     }
     getToken();
-  }, []);
+
+    return () => {
+      isRequested.current = true;
+    };
+  }, [isRequested, params]);
 
   return <div id="kakaoIdLogin">KakaoLoginPage</div>;
 };
